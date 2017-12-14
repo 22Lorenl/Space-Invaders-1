159f6da380ede5f1b86f45c68af2584cad3c3462
Golden-Leaf <33530074+Golden-Leaf@users.noreply.github.com>
2017-12-14 15:55:54 -0500

    Ball Game with Paddle

--- space_invader.py
+++ space_invader.py
@@ -36,8 +36,38 @@ class Ball:
       self.x = 3
     if pos[2] >= self.canvas_width:
       self.x = -3
- 
-ball = Ball(canvas, 'blue')
+
+class Paddle:
+  def __init__(self, canvas, color):
+    self.canvas = canvas
+    self.id = canvas.create_rectangle(20, 20, 100, 20, fill=color)
+    self.x = 0
+    self.canvas_width = self.canvas.winfo_width()
+    self.canvas.bind_all('KeyPress-a>', self.turn_left)
+    self.canvas.bind_all('KeyPress-d>', self.turn_right)
+    self.canvas.bind_all('KeyPress-s>', self.forward)
+    self.canvas.bind_all('KeyPress-d>', self.backwards)
+   def draw(self):
+    self.canvas.move(self.id, self.x, 0)
+    pos = self.canvas.coords(self.id)
+    if pos [0] <=0:
+      self.x = 0
+    elif pos [2] >= self.canvas_width:
+      self.x = 0
+   def turn_left(self, evt):
+    self.x = -2
+   def turn_right(self, evt):
+    self.x = 2
+   def forward(self, evt):
+    self.y = 2
+   def forward(self, evt):
+    self.y = -2
+
+r = random.randint(0,255)
+b = random.randint(0,255)
+g = random.randint(0,255)
+
+ball = Ball(canvas, (r,g,b) )
 
 while 1:
   ball.draw()
